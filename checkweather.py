4a4328f3c315b1e83d25ac38458950c513eb95bc
unknown <sabiennguyen@gmail.com>
2022-11-03 11:58:27 -0700

    display current temp and not max temp and add user input

--- checkweather.py
+++ checkweather.py
@@ -3,13 +3,13 @@ from pynotifier import Notification
 
 key = "8633dfbc490a72a27d9a540213feb8ad"
 url = "https://api.openweathermap.org/data/2.5/weather?q="
-name = "riverside"
+name = input()
 
 data = requests.get(url + name + '&appid=' + key).json()
 # data = requests.request("GET", url, headers=headers, params=querystring)
 city = data['name']
 country = data['sys']['country']
-temperature = (data['main']['temp_max'] - 273.15) * (9/5) + 32
+temperature = (data['main']['temp'] - 273.15) * (9.0/5.0) + 32
 weather = data['weather'][0]['main']
 humidity = data['main']['humidity']
 pressure = data['main']['pressure']
